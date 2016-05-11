c73cffdff6fb921ed7cea85896cbc9b02953abbc
Shilin Wu <wushilin@google.com>
2016-05-11 14:22:56 +0800

    added doc

--- redo.go
+++ redo.go
@@ -8,6 +8,7 @@ import (
 type JobThatMayFail func() (interface{}, error)
 type BackoffFunc func(n int) int
 
+// Returns a backoff func that sleeps 2^i exponentially with initial sleep of initial delay seconds
 func ExponentialBackoff(initialDelaySeconds int, maxSeconds int) BackoffFunc {
   return func(i int) int {
     to_sleep := int(math.Pow(2.0, float64(i))) * 1000 * initialDelaySeconds
@@ -19,12 +20,15 @@ func ExponentialBackoff(initialDelaySeconds int, maxSeconds int) BackoffFunc {
   }
 }
 
+// Returns a backoff func that sleeps fixed backoff seconds (by parameter seconds)
 func FixedBackoff(seconds int) BackoffFunc {
   return func(i int) int {
     return seconds * 1000
   }
 }
 
+
+// retry a job that may fail, up to nRetries, with backoff policy specified by BackoffFunc. Returns the job's result, nth try (0 if first try succeeded) and an error if after nRetries it still failed. The returned error will be the last error
 func Redo(f JobThatMayFail, nRetries int, backoffFunc BackoffFunc) (interface{}, int, error) {
   var err error
   var i int
